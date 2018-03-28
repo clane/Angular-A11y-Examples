1df6bbe2bbb6c63782cdb06ca81e3f76209f7296
Chris Lane <chris.lane359@gmail.com>
2018-03-28 15:00:55 -0700

    adding new CLI project

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('ng-a11y-examples App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
